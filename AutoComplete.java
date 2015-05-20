7e6b6fbc1f761b838088e811470913f3f366e674
Milap Naik <milapnaik@gmail.com>
2015-05-19 23:03:11 -0400

    Started Project

---
+++ AutoComplete.java
@@ -0,0 +1,29 @@
+import java.util.List;
+import java.util.Scanner;
+
+
+public class AutoComplete extends Trie implements IAutocomplete{
+
+	public static void main(){
+		Scanner input = new Scanner(System.in);
+		
+		
+		input.close();
+	}
+	
+	public static void getCandidates(){
+		
+	}
+
+	@Override
+	public List getCandidates(String prefix) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void pickCandidate(String prefix, String candidate) {
+		// TODO Auto-generated method stub
+		
+	}
+}
